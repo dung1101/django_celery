d79a9847787155436a4cd83e0adf5e29b60c0d86
unknown <mrbeo1221119@gmail.com>
2019-03-04 14:47:42 +0700

    up

--- new_app/views.py
+++ new_app/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .tasks import add
 
 # Create your views here.
 
 
 def index(request):
+    add.delay(1, 4)
     HttpResponse('Hello')
